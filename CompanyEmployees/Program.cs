90e01c8987307fda9e397616255348e8d56165db
Arslan Ahmad <arslan.ahmad@netwrix.com>
2023-11-20 12:04:40 +0500

    Basic services and pipeline.

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,28 @@
+using CompanyEmployees.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new()
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
